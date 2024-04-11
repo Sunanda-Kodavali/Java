6ec4c8e86835f312a67701072b1b8dbf330079a2
SunandaKodavali <sunanda.kodavali@gmail.com>
2024-04-11 13:30:32 +0200

    modified random number

--- Java_Collections/src/ArrayList/RandomNumber.java
+++ Java_Collections/src/ArrayList/RandomNumber.java
@@ -1,4 +1,20 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class RandomNumber {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        ArrayList<Integer> uniqueNumbers = new ArrayList<>();
+
+        while (uniqueNumbers.size() < 10){
+            int newNumber = random.nextInt(1, 11);
+            if(!uniqueNumbers.contains(newNumber)){
+                uniqueNumbers.add(newNumber);
+            }
+        }
+        System.out.println(uniqueNumbers);
+    }
 }
